c1ff9dd163480e1aa8d1a4400d94347bdac30006
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2024-09-28 19:08:37 +0600

    project done

--- newspaperClient/src/components/News/News.tsx
+++ newspaperClient/src/components/News/News.tsx
@@ -7,7 +7,6 @@ import userContext from "../../context/UserContext";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 
-
 interface NewsItem {
   id: number;
   headline: string;
@@ -26,7 +25,7 @@ const News = () => {
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   
   useEffect(() => {
     const socket = io("http://localhost:3000");
@@ -34,6 +33,7 @@ const News = () => {
     socket.on("news", (newsItem: NewsItem) => {
       setNews((prevNews) => [newsItem, ...prevNews]);
     });
+
 
     return () => {
       socket.disconnect();
@@ -49,14 +49,21 @@ const News = () => {
   useEffect(() => {
     if (page > 1) fetchNews(page, Query);
   }, [page]);
+
   
+
   const fetchNews = async (page: number, query: string) => {
     setLoading(true);
     try {
       const response = await fetch(`http://localhost:3000/api/news?page=${page}&search=${query}`);
       const data = await response.json();
+      // setNews((prev) => [...prev, ...data.news]);
+      
+      // setHasMore(data.news.length > 0);
       if (data.news && data.news.length > 0) {
-        setNews((prev) => [...prev, ...data.news].filter(news => news.id !== deletedId));
+        setNews((prev) => [...prev, ...data.news]);
+        setNews((prevNews) => prevNews.filter((news) => news.id != deletedId))
+   
         setHasMore(data.news.length > 0);
         setDeletedId(null);
       } else {
@@ -137,44 +144,44 @@ const News = () => {
         </div>
 
         {/* Fourth and Fifth News Articles in Card Style */}
- 
         <div className="col-span-6 flex flex-wrap">
-  {fourthFeature && (
-    <div className="w-full md:w-1/2 p-2">
-      <div className="card card-side bg-base-100 shadow-xl h-full" onClick={()=>{navigate(`/news/${fourthFeature.id}`)}}>
-        <figure className="w-1/2">
-          <img
-            src={getImageUrl(fourthFeature.image) || fallbackImage}
-            alt={fourthFeature.headline}
-            className="w-full h-full object-cover"
-          />
-        </figure>
-        <div className="card-body w-1/2">
-          <h2 className="card-title">{fourthFeature.headline}</h2>
-          <p className="line-clamp-5">{fourthFeature.details}</p>
-        </div>
-      </div>
-    </div>
-  )}
+          {fourthFeature && (
+            <div className="w-full md:w-1/2 p-2">
+              <div className="card card-side bg-base-100 shadow-xl h-full" onClick={() => { navigate(`/news/${fourthFeature.id}`) }}>
+                <figure className="w-1/2">
+                  <img
+                    src={getImageUrl(fourthFeature.image) || fallbackImage}
+                    alt={fourthFeature.headline}
+                    className="w-full h-full object-cover"
+                  />
+                </figure>
+                <div className="card-body w-1/2">
+                  <h2 className="card-title">{fourthFeature.headline}</h2>
+                  <p className="line-clamp-5">{fourthFeature.details}</p>
+                </div>
+              </div>
+            </div>
+          )}
 
-  {fifthFeature && (
-    <div className="w-full md:w-1/2 p-2" onClick={()=>{navigate(`/news/${fifthFeature.id}`);}}>
-      <div className="card card-side bg-base-100 shadow-xl h-full">
-        <figure className="w-1/2">
-          <img
-            src={getImageUrl(fifthFeature.image) || fallbackImage}
-            alt={fifthFeature.headline}
-            className="w-full h-full object-cover"
-          />
-        </figure>
-        <div className="card-body w-1/2">
-          <h2 className="card-title">{fifthFeature.headline}</h2>
-          <p className="line-clamp-5">{fifthFeature.details}</p>
+          {fifthFeature && (
+            <div className="w-full md:w-1/2 p-2" onClick={() => { navigate(`/news/${fifthFeature.id}`); }}>
+              <div className="card card-side bg-base-100 shadow-xl h-full">
+                <figure className="w-1/2">
+                  <img
+                    src={getImageUrl(fifthFeature.image) || fallbackImage}
+                    alt={fifthFeature.headline}
+                    className="w-full h-full object-cover"
+                  />
+                </figure>
+                <div className="card-body w-1/2">
+                  <h2 className="card-title">{fifthFeature.headline}</h2>
+                  <p className="line-clamp-5">{fifthFeature.details}</p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-    </div>
-  )}
-</div>
+
         {/* Remaining Articles */}
         <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 col-span-6 mt-5">
           {restNews.map((newsItem) => (
